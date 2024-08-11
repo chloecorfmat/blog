501078c748adab5e7c3cd529e93f71b7d313141d
Chloé Corfmat <contact.chloecorfmat@gmail.com>
2024-08-11 14:11:56 +0200

    Fix bug on related articles

--- wordpress/wp-content/themes/chloecorfmat/parts/block-related-articles.php
+++ wordpress/wp-content/themes/chloecorfmat/parts/block-related-articles.php
@@ -9,20 +9,21 @@
             <h2><?php echo the_field('related_articles_title') ?></h2>
             <ul class="related-articles">
                 <?php foreach ($articles as $article_object) {
-                    $article = $article_object['article']->to_array();
-                    $packshot = get_field( "article_packshot", $article['ID'] )[0];
+                    $article = $article_object['article'];
+                    $articleArray = $article->to_array();
+                    $packshot = get_field( "article_packshot", $articleArray['ID'] )[0];
                 ?>
                     <li class="article-item" data-expand-target>
-                        <a href="#" class="article-item__link" data-expand-link>
+                        <a href="<?php echo esc_url(get_permalink($article)) ?>" class="article-item__link" data-expand-link>
                             <img src="<?php echo esc_url( $packshot['sizes']['medium_large'] ) ?>" alt="" class="article-item__image" />
                             <div class="article-item__infos">
-                                <?php $terms = get_the_terms($article['ID'], 'post-main-thematic');
+                                <?php $terms = get_the_terms($articleArray['ID'], 'post-main-thematic');
                                 foreach ($terms as $term) { ?>
                                     <span class="tag"><?php echo $term->to_array()['name'] ?></span>
                                 <?php } ?>
 
-                                <h3 class="article-item__title"><?php echo $article['post_title'] ?></h3>
-                                <p class="article-item__published_at">Écrit le <time datetime="<?php echo $article['post_date'] ?>"><?php echo get_the_date('d F Y', $article) ?></time></p>
+                                <h3 class="article-item__title"><?php echo $articleArray['post_title'] ?></h3>
+                                <p class="article-item__published_at">Écrit le <time datetime="<?php echo get_the_date('c', $article) ?>"><?php echo get_the_date('j F o', $article) ?></time></p>
                             </div>
                         </a>
                     </li>
