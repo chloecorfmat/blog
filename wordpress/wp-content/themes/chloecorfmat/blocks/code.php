31a74b56821ebb1e25dbf55ddbb4010fd56a6ce5
Chloé Corfmat <contact.chloecorfmat@gmail.com>
2024-08-12 11:59:45 +0200

    fix : bug about code html in code block

--- wordpress/wp-content/themes/chloecorfmat/blocks/code.php
+++ wordpress/wp-content/themes/chloecorfmat/blocks/code.php
@@ -1,6 +1,6 @@
 <div class="gutenberg__block block-code">
     <p class="block-code__file">Fichier : <span class="block-code__filename"><?php echo(get_field('gut_code_filename')) ?></span></p>
     <pre>
-        <code class="<?php echo(get_field('gut_code_language')) ?>"><?php echo(get_field('gut_code_code')) ?></code>
+        <code class="<?php echo(get_field('gut_code_language')) ?>"><?php echo(esc_html(get_field('gut_code_code'))) ?></code>
     </pre>
 </div>
